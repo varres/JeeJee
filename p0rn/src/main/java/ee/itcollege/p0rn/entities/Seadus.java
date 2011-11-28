4a884e67dd2d6bf03bfdf9a28f3b31211c8d5d72
Tanel Liiv <tliiv@itcollege.ee>
2011-11-28 23:46:05 +0200

    Add Seadus entity

---
+++ p0rn/src/main/java/ee/itcollege/p0rn/entities/Seadus.java
@@ -0,0 +1,66 @@
+package ee.itcollege.p0rn.entities;
+
+import org.springframework.roo.addon.entity.RooEntity;
+import org.springframework.roo.addon.javabean.RooJavaBean;
+import org.springframework.roo.addon.tostring.RooToString;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.Date;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import org.springframework.format.annotation.DateTimeFormat;
+
+@RooJavaBean
+@RooToString
+@RooEntity
+public class Seadus {
+
+    private int seaduse_ID;
+
+    @NotNull
+    @Size(max = 20)
+    private String kood;
+
+    @NotNull
+    @Size(max = 20)
+    private String nimetus;
+
+    @NotNull
+    @Size(max = 20)
+    private String kehtiv_alates;
+
+    @NotNull
+    @Size(max = 20)
+    private String kehtiv_kuni;
+
+    @NotNull
+    @Size(max = 20)
+    private String kommentaar;
+
+    @NotNull
+    @Size(max = 32)
+    private String avaja;
+
+    @NotNull
+    @Temporal(TemporalType.TIMESTAMP)
+    @DateTimeFormat(style = "M-")
+    private Date avatud;
+
+    @NotNull
+    @Size(max = 32)
+    private String muutja;
+
+    @NotNull
+    @Temporal(TemporalType.TIMESTAMP)
+    @DateTimeFormat(style = "M-")
+    private Date muudetud;
+
+    @NotNull
+    @Size(max = 32)
+    private String sulgeja;
+
+    @NotNull
+    @Temporal(TemporalType.TIMESTAMP)
+    @DateTimeFormat(style = "M-")
+    private Date suletud;
+}
