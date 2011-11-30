d54fde85c8e02fea29a79f76c95186c3c549a247
Taavi Ilves <taavi@7blaze.com>
2011-11-30 22:52:34 +0200

    sql test9

--- src/test/test.sql
+++ src/test/test.sql
@@ -1,5 +1,5 @@
-INSERT INTO "Riik" (ISO_kood, ANSI_kood, Avaja, Avatud, Muutja, Muudetud, Sulgeja, Suletud, Kommentaar) VALUES ('EE', 'EST', 'Test', '2011/12/01', 'Test', '2011/12/01', '', '2030/12/01', '');
-INSERT INTO "Riik" (ISO_kood, ANSI_kood, Avaja, Avatud, Muutja, Muudetud, Sulgeja, Suletud, Kommentaar) VALUES ('EN', 'ENG', 'Test', '2011/12/01', 'Test', '2011/12/01', '', '2030/12/01', '');
-INSERT INTO "Riik" (ISO_kood, ANSI_kood, Avaja, Avatud, Muutja, Muudetud, Sulgeja, Suletud, Kommentaar) VALUES ('DE', 'DEU', 'Test', '2011/12/01', 'Test', '2011/12/01', '', '2030/12/01', '');
-INSERT INTO "Riik" (ISO_kood, ANSI_kood, Avaja, Avatud, Muutja, Muudetud, Sulgeja, Suletud, Kommentaar) VALUES ('US', 'USA', 'Test', '2011/12/01', 'Test', '2011/12/01', '', '2030/12/01', '');
+INSERT INTO "Riik" ("Riik_ID", "ISO_kood", "ANSI_kood", "Avaja", "Avatud", "Muutja", "Muudetud", "Sulgeja", "Suletud", "Kommentaar") VALUES(null, 'EE', 'EST', 'Test', '2011/12/01', 'Test', '2011/12/01', '', '2030/12/01', '');
+INSERT INTO "Riik" ("Riik_ID", "ISO_kood", "ANSI_kood", "Avaja", "Avatud", "Muutja", "Muudetud", "Sulgeja", "Suletud", "Kommentaar") VALUES(null, 'EN', 'ENG', 'Test', '2011/12/01', 'Test', '2011/12/01', '', '2030/12/01', '');
+INSERT INTO "Riik" ("Riik_ID", "ISO_kood", "ANSI_kood", "Avaja", "Avatud", "Muutja", "Muudetud", "Sulgeja", "Suletud", "Kommentaar") VALUES(null, 'DE', 'DEU', 'Test', '2011/12/01', 'Test', '2011/12/01', '', '2030/12/01', '');
+INSERT INTO "Riik" ("Riik_ID", "ISO_kood", "ANSI_kood", "Avaja", "Avatud", "Muutja", "Muudetud", "Sulgeja", "Suletud", "Kommentaar") VALUES(null, 'US', 'USA', 'Test', '2011/12/01', 'Test', '2011/12/01', '', '2030/12/01', '');
 COMMIT;
