e91543cd6f1c1be89f67d14d90f3f6a9f8d23aeb
Taavi Ilves <taavi@7blaze.com>
2011-11-30 23:14:16 +0200

    sql test13

--- src/test/test.sql
+++ src/test/test.sql
@@ -1,3 +1,22 @@
+CREATE TABLE RIIK (
+       riik_ID              INTEGER,
+       avaja                VARCHAR(32) NOT NULL,
+       avatud               DATE NOT NULL,
+       muutja               VARCHAR(32) NOT NULL,
+       muudetud             DATE NOT NULL,
+       sulgeja              VARCHAR(32),
+       suletud              DATE NOT NULL,
+       ISO_kood             VARCHAR(20) NOT NULL,
+       kommentaar           LONG VARCHAR,
+       ANSI_kood            CHAR(18),
+       PRIMARY KEY (riik_ID)
+);
+
+CREATE UNIQUE INDEX XPKRIIK ON RIIK
+(
+       riik_ID
+);
+
 INSERT INTO RIIK (riik_ID, ISO_kood, ANSI_kood, avaja, avatud, muutja, muudetud, sulgeja, suletud, kommentaar) VALUES(null, 'EE', 'EST', 'Test', '2011-12-01 00:00:00', 'Test', '2011-12-01 00:00:00', '', '2030-12-01 00:00:00', '');
 INSERT INTO RIIK (riik_ID, ISO_kood, ANSI_kood, avaja, avatud, muutja, muudetud, sulgeja, suletud, kommentaar) VALUES(null, 'EN', 'ENG', 'Test', '2011-12-01 00:00:00', 'Test', '2011-12-01 00:00:00', '', '2030-12-01 00:00:00', '');
 INSERT INTO RIIK (riik_ID, ISO_kood, ANSI_kood, avaja, avatud, muutja, muudetud, sulgeja, suletud, kommentaar) VALUES(null, 'DE', 'DEU', 'Test', '2011-12-01 00:00:00', 'Test', '2011-12-01 00:00:00', '', '2030-12-01 00:00:00', '');
