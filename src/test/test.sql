873131d9fd58a0846364efac2007da7251de9dae
Taavi Ilves <taavi@7blaze.com>
2011-11-30 22:49:07 +0200

    sql test8

--- src/test/test.sql
+++ src/test/test.sql
@@ -1,5 +1,5 @@
-INSERT INTO Riik o (ISO_kood, ANSI_kood, Avaja, Avatud, Muutja, Muudetud, Sulgeja, Suletud, Kommentaar) VALUES ('EE', 'EST', 'Test', '2011/12/01', 'Test', '2011/12/01', '', '2030/12/01', '');
-INSERT INTO Riik o (ISO_kood, ANSI_kood, Avaja, Avatud, Muutja, Muudetud, Sulgeja, Suletud, Kommentaar) VALUES ('EN', 'ENG', 'Test', '2011/12/01', 'Test', '2011/12/01', '', '2030/12/01', '');
-INSERT INTO Riik o (ISO_kood, ANSI_kood, Avaja, Avatud, Muutja, Muudetud, Sulgeja, Suletud, Kommentaar) VALUES ('DE', 'DEU', 'Test', '2011/12/01', 'Test', '2011/12/01', '', '2030/12/01', '');
-INSERT INTO Riik o (ISO_kood, ANSI_kood, Avaja, Avatud, Muutja, Muudetud, Sulgeja, Suletud, Kommentaar) VALUES ('US', 'USA', 'Test', '2011/12/01', 'Test', '2011/12/01', '', '2030/12/01', '');
+INSERT INTO "Riik" (ISO_kood, ANSI_kood, Avaja, Avatud, Muutja, Muudetud, Sulgeja, Suletud, Kommentaar) VALUES ('EE', 'EST', 'Test', '2011/12/01', 'Test', '2011/12/01', '', '2030/12/01', '');
+INSERT INTO "Riik" (ISO_kood, ANSI_kood, Avaja, Avatud, Muutja, Muudetud, Sulgeja, Suletud, Kommentaar) VALUES ('EN', 'ENG', 'Test', '2011/12/01', 'Test', '2011/12/01', '', '2030/12/01', '');
+INSERT INTO "Riik" (ISO_kood, ANSI_kood, Avaja, Avatud, Muutja, Muudetud, Sulgeja, Suletud, Kommentaar) VALUES ('DE', 'DEU', 'Test', '2011/12/01', 'Test', '2011/12/01', '', '2030/12/01', '');
+INSERT INTO "Riik" (ISO_kood, ANSI_kood, Avaja, Avatud, Muutja, Muudetud, Sulgeja, Suletud, Kommentaar) VALUES ('US', 'USA', 'Test', '2011/12/01', 'Test', '2011/12/01', '', '2030/12/01', '');
 COMMIT;
