0acd3c6661985b9a418f975bfd2c7eed2586e594
Taavi Ilves <taavi@7blaze.com>
2011-11-30 22:38:51 +0200

    sql test3

--- src/test/test.sql
+++ src/test/test.sql
@@ -1,1 +1,10 @@
-INSERT INTO Riik SET ISO_kood = 'EE', ANSI_kood = 'ET', avaja = 'Test', avatud = CURDATE(), muutja = 'Test', muudetud = CURDATE(), sulgeja = '', suletud = '2020/12/20';
+TRUNCATE TABLE Riik;
+INSERT INTO Riik ("ISO_kood", "ANSI_kood", "Avaja", "Avatud", "Muutja", "Muudetud", "Sulgeja", "Suletud", "Kommentaar")
+VALUES ('EE', 'EST', 'Test', '2011/12/01', 'Test', '2011/12/01', '', '2030/12/01', '');
+INSERT INTO Riik ("ISO_kood", "ANSI_kood", "Avaja", "Avatud", "Muutja", "Muudetud", "Sulgeja", "Suletud", "Kommentaar")
+VALUES ('EN', 'ENG', 'Test', '2011/12/01', 'Test', '2011/12/01', '', '2030/12/01', '');
+INSERT INTO Riik ("ISO_kood", "ANSI_kood", "Avaja", "Avatud", "Muutja", "Muudetud", "Sulgeja", "Suletud", "Kommentaar")
+VALUES ('DE', 'DEU', 'Test', '2011/12/01', 'Test', '2011/12/01', '', '2030/12/01', '');
+INSERT INTO Riik ("ISO_kood", "ANSI_kood", "Avaja", "Avatud", "Muutja", "Muudetud", "Sulgeja", "Suletud", "Kommentaar")
+VALUES ('US', 'USA', 'Test', '2011/12/01', 'Test', '2011/12/01', '', '2030/12/01', '');
+COMMIT;
