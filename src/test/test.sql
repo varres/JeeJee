5cfe89f8d09633ffc78fcc47c476bb9ec46349f3
Taavi Ilves <taavi@7blaze.com>
2011-11-30 23:11:45 +0200

    sql test12

--- src/test/test.sql
+++ src/test/test.sql
@@ -2,4 +2,3 @@ INSERT INTO RIIK (riik_ID, ISO_kood, ANSI_kood, avaja, avatud, muutja, muudetud,
 INSERT INTO RIIK (riik_ID, ISO_kood, ANSI_kood, avaja, avatud, muutja, muudetud, sulgeja, suletud, kommentaar) VALUES(null, 'EN', 'ENG', 'Test', '2011-12-01 00:00:00', 'Test', '2011-12-01 00:00:00', '', '2030-12-01 00:00:00', '');
 INSERT INTO RIIK (riik_ID, ISO_kood, ANSI_kood, avaja, avatud, muutja, muudetud, sulgeja, suletud, kommentaar) VALUES(null, 'DE', 'DEU', 'Test', '2011-12-01 00:00:00', 'Test', '2011-12-01 00:00:00', '', '2030-12-01 00:00:00', '');
 INSERT INTO RIIK (riik_ID, ISO_kood, ANSI_kood, avaja, avatud, muutja, muudetud, sulgeja, suletud, kommentaar) VALUES(null, 'US', 'USA', 'Test', '2011-12-01 00:00:00', 'Test', '2011-12-01 00:00:00', '', '2030-12-01 00:00:00', '');
-COMMIT;
