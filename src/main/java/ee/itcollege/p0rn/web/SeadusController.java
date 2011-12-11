ff989bede4d632b0fbee6d9ef945c04d8f602b51
maarek <maarek@maarekPC.lan>
2011-12-11 13:35:25 +0200

    Koodi dubleerimine eemaldatud

--- src/main/java/ee/itcollege/p0rn/web/SeadusController.java
+++ src/main/java/ee/itcollege/p0rn/web/SeadusController.java
@@ -31,12 +31,11 @@ public class SeadusController {
 	
     @RequestMapping(method = RequestMethod.POST)
     public String create(@Valid Seadus seadus, BindingResult bindingResult, Model uiModel, HttpServletRequest httpServletRequest) {
+    	uiModel.addAttribute("seadus", seadus);
     	if (bindingResult.hasErrors()) {
-        	uiModel.addAttribute("seadus", seadus);
             addDateTimeFormatPatterns(uiModel);
             return "seaduses/create";
         }
-    	uiModel.addAttribute("seadus", seadus);
         uiModel.asMap().clear();
         seadus.persist();
         return "redirect:/seaduses/" + encodeUrlPathSegment(seadus.getId().toString(), httpServletRequest);
